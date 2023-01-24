c6a677500eeccb3c5dc0b6294b0e312c23a7c3e8
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-01-24 18:49:21 +0900

    変数を減らして、コードを簡略化。

--- electron-app/renderer.js
+++ electron-app/renderer.js
@@ -1,41 +1,32 @@
-/** @type {HTMLElement} */
-const information = document.getElementById('info');
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+document.getElementById('info').innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
 
-/** @type {HTMLElement} */
-const dirName = document.getElementById('dir-name');
 paths.dirName()
   .then(response => {
-    dirName.innerText = response;
+    document.getElementById('dir-name').innerText = response;
   });
 
-/** @type {HTMLElement} */
-const resourcesPath = document.getElementById('resources-path');
 paths.resourcesPath()
   .then(response => {
-    resourcesPath.innerText = response;
+    document.getElementById('resources-path').innerText = response;
   });
 
-/** @type {HTMLElement} */
-const edgeNative = document.getElementById('edge-native');
-edgeNative.innerText = paths.edgeNative();
+document.getElementById('edge-native').innerText = paths.edgeNative();
 
-/** @type {HTMLInputElement} */
-const greetingButton = document.getElementById('greeting-button');
-greetingButton.addEventListener('click', (ev) => {
+document.getElementById('greeting-button')
+  .addEventListener('click', (ev) => {
 
-  /** @type {HTMLInputElement} */
-  const greetingTo = document.getElementById('greeting-to');
+    /** @type {HTMLInputElement} */
+    const greetingTo = document.getElementById('greeting-to');
 
-  /** @type {HTMLElement} */
-  const greeting = document.getElementById('greeting');
-  sampleLib.greeting(greetingTo.value)
-    .then(
-      response => {
-        greeting.innerText = response;
-      },
-      rejected => {
-        greeting.innerText = rejected;
-      }
-    );
-});
+    /** @type {HTMLElement} */
+    const greeting = document.getElementById('greeting');
+    sampleLib.greeting(greetingTo.value)
+      .then(
+        response => {
+          greeting.innerText = response;
+        },
+        rejected => {
+          greeting.innerText = rejected;
+        }
+      );
+  });
